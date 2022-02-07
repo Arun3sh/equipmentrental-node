eb2b0b65ae44ed6df01448aa2fac94a9c9e7cfbe
Arun3sh <m.aruneshwar@gmail.com>
2022-02-08 01:02:46 +0530

    removed unwanted

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,10 @@ import express, { request, response } from 'express';
 import { addUsers, findUsername, genPassword } from '../helper.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import cors from 'cors';
 import { client } from './../index.js';
 import { auth } from './auth.js';
 
-// const app = express();
-// app.use(cors());
 const router = express.Router();
-
-// app.use(express.json()) -- middleware
 
 router.get('/', auth, async (request, response) => {
 	const filter = request.query;
@@ -24,8 +19,8 @@ router.get('/', auth, async (request, response) => {
 // Post method to signup the user
 router.post('/signup', async (request, response) => {
 	const { username, password, email } = request.body;
-	console.log(username);
-	const checkUsername = await findUsername({ username: username });
+
+	const checkUsername = await findUsername({ email: email });
 
 	// To check password strength
 	const passwordTester = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})');
@@ -35,7 +30,7 @@ router.post('/signup', async (request, response) => {
 		response.status(400).send('Username already taken');
 		return;
 	}
-	console.log(strength);
+
 	if (!strength) {
 		response
 			.status(400)
@@ -53,7 +48,7 @@ router.post('/signup', async (request, response) => {
 
 router.post('/login', async (request, response) => {
 	const { username, password } = request.body;
-	console.log('username', username);
+
 	const checkUsername = await findUsername({ username: username });
 
 	if (checkUsername === null) {
@@ -61,17 +56,18 @@ router.post('/login', async (request, response) => {
 		response.status(401).send('Username / Password incorrect');
 		return;
 	}
+
 	const checkPassword = await bcrypt.compare(password, checkUsername.password);
+
 	if (checkPassword) {
 		const token = jwt.sign({ id: checkUsername._id }, process.env.SECRET_KEY);
+
 		response.header('x-auth-token', token).send({ token: token });
-		// return;
 	} else {
 		// 401 is for unauthorized
 		response.status(401).send('Username/Password incorrect');
 		return;
 	}
-	// response.send('Logged in');
 });
 
 export const usersRouter = router;
