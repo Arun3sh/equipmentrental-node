4ab7c217b6048fc05978eed5aeefb1081cbf8a7f
Arun3sh <m.aruneshwar@gmail.com>
2022-02-28 19:15:58 +0530

    methods added

--- helper.js
+++ helper.js
@@ -2,6 +2,7 @@ import { client } from './index.js';
 import bcrypt from 'bcrypt';
 import { ObjectId } from 'mongodb';
 
+// Users
 // Find all users
 async function findAllUsers(filter) {
 	return await client.db('mern').collection('users').find(filter).toArray();
@@ -22,7 +23,7 @@ async function addUsers(data) {
 
 // Find user email
 async function findUserEmail(filter) {
-	return client.db('mern').collection('users').findOne(filter);
+	return await client.db('mern').collection('users').findOne(filter);
 }
 
 async function genPassword(password) {
@@ -38,7 +39,55 @@ async function updateUserOrder(id, updateOrder) {
 	return await client
 		.db('mern')
 		.collection('users')
-		.updateOne({ id: id }, { $push: { orders: updateOrder } });
+		.updateOne({ _id: ObjectId(id) }, { $push: { orders: updateOrder } });
 }
 
-export { findAllUsers, findUserWithId, findUserEmail, addUsers, genPassword, updateUserOrder };
+// ----- Products -----
+
+// To get all products
+async function getAllProducts(filter) {
+	return await client.db('mern').collection('products').find(filter).sort({ name: 1 }).toArray();
+}
+
+// To get product by Id
+async function getProductById(id) {
+	return await client
+		.db('mern')
+		.collection('products')
+		.findOne({ _id: ObjectId(id) });
+}
+
+//To add product
+async function addProduct(data) {
+	return await client.db('mern').collection('products').insertMany([data]);
+}
+
+// To edit product
+async function editProduct(id, updatedProduct) {
+	return await client
+		.db('mern')
+		.collection('products')
+		.updateOne({ _id: ObjectId(id) }, { $set: updatedProduct });
+}
+
+// To delete product
+async function deleteProduct(id) {
+	return await client
+		.db('mern')
+		.collection('products')
+		.deleteOne({ _id: ObjectId(id) });
+}
+
+export {
+	findAllUsers,
+	findUserWithId,
+	findUserEmail,
+	addUsers,
+	genPassword,
+	updateUserOrder,
+	getAllProducts,
+	getProductById,
+	addProduct,
+	editProduct,
+	deleteProduct,
+};
