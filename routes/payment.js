7da5033cb4c1734ce744fb5e1917dc6cd4d427f9
Arun3sh <m.aruneshwar@gmail.com>
2022-03-10 23:34:55 +0530

    try2

--- routes/payment.js
+++ routes/payment.js
@@ -7,33 +7,32 @@ import { updateUserOrder } from '../helper.js';
 const router = express.Router();
 
 // For creating order
-router.post('/order', auth, async (request, response, next) => {
-	// try {
-	const { amount, currency } = request.body;
-	const myinstance = new Razorpay({
-		key_id: process.env.RAZORPAY_KEY_ID,
-		key_secret: process.env.RAZORPAY_SECRET,
-	});
+router.post('/order', auth, async (request, response) => {
+	try {
+		const { amount, currency } = request.body;
+		const myinstance = new Razorpay({
+			key_id: process.env.RAZORPAY_KEY_ID,
+			key_secret: process.env.RAZORPAY_SECRET,
+		});
 
-	const options = {
-		amount: amount * 100, // amount in smallest currency unit
-		currency: currency,
-		payment_capture: '1',
-	};
+		const options = {
+			amount: amount * 100, // amount in smallest currency unit
+			currency: currency,
+			payment_capture: '1',
+		};
 
-	await myinstance.orders.create(options, (err, order) => {
-		if (!err) {
-			console.log(order);
-			response.json(order);
-		} else {
-			console.log(err);
-			response.send({ error: err });
-		}
-	});
-
-	// } catch (error) {
-	// 	response.status(500).send({ error: error });
-	// }
+		await myinstance.orders.create(options, (err, order) => {
+			if (!err) {
+				console.log(order);
+				response.json(order);
+			} else {
+				console.log(err);
+				response.send({ error: err });
+			}
+		});
+	} catch (error) {
+		response.status(500).send({ error: error });
+	}
 });
 
 router.post('/success', auth, async (req, res) => {
